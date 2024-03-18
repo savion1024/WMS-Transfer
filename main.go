70a741f25f1308205fa702f9dd446e7dc72e66ef
savion <978934842@qq.com>
2024-03-18 13:04:51 +0800

    feat: delete database

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"wxcloudrun-golang/db"
 	"wxcloudrun-golang/service"
 )
 
 func main() {
-	if err := db.Init(); err != nil {
-		panic(fmt.Sprintf("mysql init failed with %+v", err))
-	}
+	//if err := db.Init(); err != nil {
+	//	panic(fmt.Sprintf("mysql init failed with %+v", err))
+	//}
 
 	http.HandleFunc("/", service.IndexHandler)
 	http.HandleFunc("/api/count", service.CounterHandler)
